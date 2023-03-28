e0d780c77fb204abdb41b89bb558146ee64564bc
Hyoseung.jeon <qmflwj1441@gmail.com>
2023-03-28 17:47:28 +0900

    Update OrderListDto  - import 정리

--- src/main/java/jpabook/jpashop/domain/dto/OrderListDto.java
+++ src/main/java/jpabook/jpashop/domain/dto/OrderListDto.java
@@ -1,17 +1,13 @@
 package jpabook.jpashop.domain.dto;
 
 import jpabook.jpashop.domain.*;
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
-
-import static javax.persistence.FetchType.LAZY;
 
 @Getter
 @Setter
@@ -26,7 +22,7 @@ public class OrderListDto {
 
     private List<OrderItemDto> orderItems = new ArrayList<>();
 
-    public OrderListDto(Order order) {
+    public  OrderListDto(Order order) {
         this.id = order.getId();
         this.orderDate = order.getOrderDate();
         this.status = order.getStatus();
